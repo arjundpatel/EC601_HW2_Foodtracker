be2d64a7a840608db096405fc2d1e6f6cc0b7652
Arjun Patel <31737983+arjundpatel@users.noreply.github.com>
2017-10-09 22:17:58 -0400

    Foodtracker

---
+++ Authentication.swift
@@ -0,0 +1,15 @@
+//
+//  Authentication.swift
+//  FoodTracker
+//
+//  Created by Arjun Patel on 10/9/17.
+//  Copyright © 2017 Apple Inc. All rights reserved.
+//
+
+import UIKit
+
+class Authentication: UIStackView {
+
+
+
+}
---
+++ MealTableViewCell.swift
@@ -0,0 +1,28 @@
+//
+//  MealTableViewCell.swift
+//  FoodTracker
+//
+//  Created by Arjun Patel on 10/9/17.
+//
+
+import UIKit
+
+class MealTableViewCell: UITableViewCell {
+    
+    //MARK: Properties
+    @IBOutlet weak var nameLabel: UILabel!
+    @IBOutlet weak var photoImageView: UIImageView!
+    @IBOutlet weak var ratingControl: RatingControl!
+
+    override func awakeFromNib() {
+        super.awakeFromNib()
+        // Initialization code
+    }
+
+    override func setSelected(_ selected: Bool, animated: Bool) {
+        super.setSelected(selected, animated: animated)
+
+        // Configure the view for the selected state
+    }
+
+}
